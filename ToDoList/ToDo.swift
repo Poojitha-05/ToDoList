67a001ddb33f7d3b1749386318c432c58d19a43a
Poojitha Panda <poojithapanda7@gmail.com>
2022-07-15 16:25:53 -0700

    Iteration 0 complete

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,19 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Scholar on 7/15/22.
+//
+
+import UIKit
+
+
+class ToDo {
+//    give name a default value
+    var name = "OH NO, IT'S BROKEN"
+    var important = true
+    
+//    init(<#parameters#>) {
+//        <#statements#>
+//    }
+}
